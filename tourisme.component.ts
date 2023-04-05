d7897601acb395c98dcf733681e46547e5598c66
megane <meganenoella40@gmail.com>
2023-04-05 08:52:38 +0200

    Add acceuil.css tourisme.component.html tourisme.component.ts

---
+++ tourisme.component.ts
@@ -0,0 +1,11 @@
+/*import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-tourisme',
+    templateUrl : './tourisme.component.html',
+    styleUrls : ['./acceuil.css']
+})
+export class tourismeAcceuil{
+
+    
+}*/
